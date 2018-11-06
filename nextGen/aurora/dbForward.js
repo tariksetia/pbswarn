cac7302ed667e51643859e1c5c9eed06963f18a4
Art Botterell <art@botterell.net>
2018-11-05 17:44:02 -0800

    Create dbForward.js
    call with 'node dbForward.js 6606 db 3306 &' or, for short, 'sh db.sh'

---
+++ nextGen/aurora/dbForward.js
@@ -0,0 +1,16 @@
+// Simple port forwarding ...
+// forward.js <local port> <forward host> <forward port>
+// i.e. "forward.js 8080 1.2.3.4 80" will forward 127.0.0.1:8080 to 1.2.3.4:80
+var net = require("net");
+
+var portIn = process.argv[2],
+    hostOut = process.argv[3],
+    portOut = process.argv[4];
+
+net.createServer(function(connIn) {
+	var connOut = net.createConnection(portOut, hostOut);
+	connIn.pipe(connOut);
+	connOut.pipe(connIn);
+	connOut.on("end", connIn.end.bind(connIn));
+	connIn.on("end", connOut.end.bind(connOut));
+}).listen(portIn);
