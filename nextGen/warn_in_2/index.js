30947e15b7dd2f7031cb3f9600143449ce3b073b
Art Botterell <art@botterell.net>
2018-11-12 21:30:12 -0800

    Better pooling

--- nextGen/warn_in_2/index.js
+++ nextGen/warn_in_2/index.js
@@ -2,12 +2,7 @@ const moment = require('moment')
 const xml2js = require('xml2js')
 const mysql = require('mysql2')
 
-// on POST call to map URL
-exports.handler = (event, context, callback) => {
-    
-    context.callbackWaitsForEmptyEventLoop = false;
-    var now, hb
-    var pool = mysql.createPool({
+var pool = mysql.createPool({
         connectionLimit : 100,
         host     : 'warn.cluster-czmkuxdhszlq.us-west-2.rds.amazonaws.com',
         port     : 3306,
@@ -18,19 +13,25 @@ exports.handler = (event, context, callback) => {
         connectionLimit: 10,
         queueLimit: 0
     })
+
+// on POST call to map URL
+exports.handler = (event, context, callback) => {
+    
+    context.callbackWaitsForEmptyEventLoop = false;
+    var now, hb, ns, uid, alert, alertExpires
+    
+    
     
     // New last-heartbeat timestamp
     now = moment().format('YYYY-MM-DD hh:mm:ssZ')
     
-    // now parse the CAP XML
-    var ns = null, uid = null, alert = null, alertExpires = ""
-    var alertXml = event.xml // set aside raw XML to post to db
+    // now parse the submitted CAP XML
     xml2js.parseString(event.xml, function (err, result) {
         if (err) console.log("XML Parse Error",err)
         alert = result.alert
         try {
             ns = alert.$.xmlns
-            uid = alert.identifier+","+alert.sender+","+alert.sent
+            uid = alert.identifier + "," + alert.sender + "," + alert.sent // per CAP spec
         } catch (e) { ns = null; uid=""; }
     });
     
@@ -42,10 +43,10 @@ exports.handler = (event, context, callback) => {
         }
     }
     
-    // if it's CAP post the new alert to DB
+    // if it's a CAP message, post the new alert to DB
     if (ns == "urn:oasis:names:tc:emergency:cap:1.2") {
         sql = "INSERT INTO warn.alerts (uid, xml, expires, received) VALUES (?,?,?,?)"
-        sql = mysql.format(sql, [uid, alertXml, alertExpires, now])
+        sql = mysql.format(sql, [uid, event.xml, alertExpires, now])
         try {
             pool.query(sql,function(error,rows) {
                 if (error) console.log("DUPLICATE", uid) // the DB will error any dupes by UID
@@ -59,13 +60,14 @@ exports.handler = (event, context, callback) => {
     sql = mysql.format(sql, now)
     pool.query(sql,function(error,results) {
         if (error) console.log("HB Update Error",error)
+        callback(error, {"statusCode":"200", "body":moment(hb).format() + " " + now})
         
-        // and then read back
+        /*// and then read back
         pool.query("select * from warn.heartbeat", function(error,results) {
             if (error) console.log("HB Readback Error",error)
             hb = results[0].latest
             callback(error, {"statusCode":"200", "body":moment(hb).format() + " " + uid})
-        })
+        })*/
     })
     
 }
