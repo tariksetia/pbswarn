aa5c46812d6e2ff39a0bc115cb5c4e1f298cfdb5
Art Botterell <art@botterell.net>
2018-11-16 15:32:50 -0800

    Separate security groups for RDS and Lambda

--- nextGen/warn_in_2/index.js
+++ nextGen/warn_in_2/index.js
@@ -4,7 +4,7 @@
  *  Contact: <warn@pbs.org>
  *  All Rights Reserved.
  *
- *  Version 1.18 11/14/2018
+ *  Version 1.18 11/16/2018
  *
  *************************************************************/
 
@@ -15,83 +15,106 @@ const xml2js = require('xml2js')
 const mysql = require('mysql2')
 const atob = require('atob')
 
-var pool =  mysql.createPool({
-                connectionLimit     : 100,
-                host                : 'warn.cluster-czmkuxdhszlq.us-west-2.rds.amazonaws.com',
-                port                : 3306,
-                database            : 'warn',
-                user                : 'warn',
-                password            : 'warnwarn',
-                waitForConnections  : true,
-                connectionLimit     : 10,
-                queueLimit          : 0
-            })
+var pool
+var callbackGlobal
+
+var db_config = {
+        connectionLimit     : 200,
+        host                : 'warn.cluster-czmkuxdhszlq.us-west-2.rds.amazonaws.com',
+        port                : 3306,
+        user                : 'warn',
+        password            : 'warnwarn',
+        waitForConnections  : true,
+        connectTimeout      : 20000,
+        queueLimit          : 0
+    }
 
 // on POST call to map URL
-exports.handler = (event, context, callback) => {
-    
-    context.callbackWaitsForEmptyEventLoop = false;
-    
-    var message = atob(event.body64)  // 'body64' config'd in API GW Resources POST template
-    
+exports.handler = async (event, context, callback) => {
+    callbackGlobal = callback
+    context.callbackWaitsForEmptyEventLoop = false  // asynchronize the handler callback
     //  update the heartbeat value on DB
     var now = moment().format('YYYY-MM-DD HH:mm:ssZ')
-    var sql = "UPDATE warn.heartbeat SET latest=? WHERE Id=1"
-    pool.execute(sql,[now], function(error,results) {
-        if (error) console.log("HB Update Error",error)
-        
-        /*// read back heartbeat from DB
-        pool.query("select * from warn.heartbeat", function(error,results) {
-            if (error) console.log("HB Readback Error",error)
-            var hb = results[0].latest
-            callback(error, {"statusCode":"200", "body":moment(hb).format() + " " + uid})
-        })*/
-        
-    })
-    
-    // if it's a heartbeat message, short-circuit the rest
+    var message = atob(event.body64)  // 'body64' config'd in API GW Resources POST template
+    await updateHeartbeat(now)
+    // if it's a labeled heartbeat message, short cut out
     if (message == 'heartbeat') {
-        callback(null, {"statusCode":"200", "body":moment(now).format()})
+        respond(context, "200", moment(now).format(), "")
         return
     }
+    // else process the POSTed XML
+    await procAlert(context, message, now)
+    return
+}
+
+async function updateHeartbeat(now) {
+    var sql = "UPDATE warn.heartbeat SET latest=? WHERE Id=1"
+    var conn = mysql.createConnection(db_config)
+    conn.execute(sql, [now], function(error, results) {
+        if (error) console.log("HB Execute Error", error)
+        conn.destroy()
+    })
+    // ensure time to complete
+    await sleep(500)
+}
+
+async function procAlert(context, message, now) {
     
-    // now parse the submitted CAP XML to JSON
     xml2js.parseString(message, function (err, result) {
         if (err) console.log("XML2JS Error",err)
-        
-        // save some values
-        var alert = result.alert  // the alert structure as JSON
+        // extract XML namespace
         var ns = ""
-        if (typeof alert.$.xmlns != 'undefined') {
-            ns = alert.$.xmlns // the XML default namespace
+        if (typeof result.alert.$.xmlns != 'undefined') {
+            ns = result.alert.$.xmlns // the XML default namespace
         }
-        var uid = alert.identifier + "," + alert.sender + "," + alert.sent // per CAP spec
-        
-        // extract the latest expires time among all Infos (assuming a CAP message)
-        var alertExpires = ""
-        if (typeof alert.info != 'undefined') {
-            for (var info of alert.info) {
-                if (info.expires > alertExpires) {
-                    alertExpires = info.expires
-                } 
-            }
-        } 
-        
         // if it's a CAP message, post the new alert to DB
         if (ns == "urn:oasis:names:tc:emergency:cap:1.2") {
-            sql = "INSERT INTO warn.alerts (uid, xml, expires, received) VALUES (?,?,?,?)"
-            pool.execute(sql,[uid, message, alertExpires, now], function(error,rows) {
-                if (error) console.log("DUPLICATE", uid, error) // the DB will error any duplicate UID
-                else console.log("ADDED", uid)
-            })
+            var alert = result.alert
+            // extract a few key fields
+            var uid = alert.identifier + "," + alert.sender + "," + alert.sent // per CAP spec
+            // extract the latest expires time across all Infos
+            var alertExpires = ""
+            if (typeof alert != 'undefined') {
+                if (typeof alert.info != 'undefined') {
+                    for (var info of alert.info) {
+                        if (info.expires > alertExpires) {
+                            alertExpires = info.expires
+                        }
+                    }
+                }
+            }
+            postAlert(uid, message, alertExpires, now)
         }
-        
-        // if it's something else, e.g., CMAM, ignore it for now
-        
-        // and reply to the HTTP call
-        callback(null, {"statusCode":"200", "body":moment(now).format() + " " + uid})
-        
-    });
-            
+    })  
+
 }
-    
+
+async function postAlert(uid, message, expires, now) {
+    var sql = "INSERT INTO warn.alerts (uid, xml, expires, received) VALUES (?,?,?,?)"
+    var conn = mysql.createConnection(db_config)
+    conn.execute(sql, [uid, message, expires, now], function(error, results) {
+        conn.destroy()
+        if (error) {
+            if (error.message.includes("Duplicate entry")) {
+                console.log("DUPLICATE", uid) // the DB will error any duplicate UID
+                respond("200", moment(now).format(), " " + "DUPLICATE " + uid)
+            } else {
+                console.log("ERROR", uid, error) // other things can go wrong
+                respond("500", moment(now).format(), " " + uid + " " + error)
+            }
+        } else {
+            console.log("ADDED", uid)
+            respond("200", moment(now).format(), " " + uid)
+        }
+    })
+    // ensure time to complete
+    await sleep(500)
+}
+
+async function respond(status, now, uid) {
+    callbackGlobal(null, {"statusCode":status, "body":moment(now).format() + " " + uid})
+}
+
+async function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
