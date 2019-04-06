f7628bd2f7593f6b89836a6837b72104fd5baf90
Art Botterell <art@botterell.net>
2019-04-05 17:15:19 -0700

    Update warndb.go

--- client/warndb.go
+++ client/warndb.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"strconv"
 	"strings"
 
 	"../cap"
@@ -11,10 +12,12 @@ import (
 )
 
 var db *sql.DB
+var alertID int64
+var infoID int64
+var areaID int64
 
 // ToDB ...
 func ToDB(alert cap.Alert, xml string) {
-	var alertID int
 	// test if duplicate
 	statement := "select count(*) from alerts where identifier ='" + alert.Identifier + "' sender = '" + alert.Sender + "' and sent = '" + alert.Sent + "'"
 
@@ -36,7 +39,7 @@ func ToDB(alert cap.Alert, xml string) {
 		if err != nil {
 			log.Println("Insert alert error:", err)
 		} else {
-			alertID, err := res.LastInsertId()
+			alertID, err = res.LastInsertId()
 			if err != nil {
 				log.Println(err)
 			}
@@ -53,7 +56,6 @@ func ToDB(alert cap.Alert, xml string) {
 			log.Println("Insert CAP error:", err)
 		}
 		// add Infos
-		var infoID int64
 		for _, info := range alert.Infos {
 			// extract CMAM value
 			cmam := getCMAM(info)
@@ -79,75 +81,57 @@ func ToDB(alert cap.Alert, xml string) {
 				log.Fatal(err)
 			}
 			defer statement.Close()
-			res, err := statement.Exec(string(alertID), info.Language, info.Category, info.Event, info.ResponseType, info.Urgency, info.Severity, info.Certainty, info.Expires, info.SenderName, slug, info.Effective, info.Onset, info.Audience, info.Headline, cmam, info.Description, info.Instruction, info.Contact, info.Web)
+			res, err := statement.Exec(strconv.FormatInt(alertID, 10), info.Language, info.Category, info.Event, info.ResponseType, info.Urgency, info.Severity, info.Certainty, info.Expires, info.SenderName, slug, info.Effective, info.Onset, info.Audience, info.Headline, cmam, info.Description, info.Instruction, info.Contact, info.Web)
 			infoID, err = res.LastInsertId()
 			if err != nil {
 				log.Println("Insert info error", err)
 			}
-			fmt.Println("Added info number", infoID)
 
 			// add Parameters
-			var paramID int64
+
 			for _, param := range info.Parameters {
 				statement, err = db.Prepare("insert into parameters (infoId, valueName, value) values (?, ?, ?)")
 				if err != nil {
 					log.Println(err)
 				}
 				defer statement.Close()
-				res, err := statement.Exec(string(infoID), param.ValueName, param.Value)
-				paramID, err = res.LastInsertId()
+				_, err := statement.Exec(strconv.FormatInt(infoID, 10), param.ValueName, param.Value)
 				if err != nil {
 					log.Println("Insert parameter error:", err)
 				}
-				fmt.Println("Added param number", paramID)
 			}
 
 			// add EventCodes
-			var eventCodeID int64
 			for _, ec := range info.EventCodes {
 				statement, err = db.Prepare("insert into eventCodes (infoId, valueName, value) values (?, ?, ?)")
 				if err != nil {
 					log.Println(err)
 				}
-				res, err = statement.Exec(string(infoID), ec.ValueName, ec.Value)
+				_, err = statement.Exec(strconv.FormatInt(infoID, 10), ec.ValueName, ec.Value)
 				if err != nil {
 					log.Fatal("Insert eventCode error:", err)
-				} else {
-					eventCodeID, err = res.LastInsertId()
-					if err != nil {
-						log.Println(err)
-					}
-					fmt.Println("Added eventCode number", eventCodeID)
 				}
 			}
 
 			// add Resources
-			var resourceID int64
 			for _, resource := range info.Resources {
 				statement, err = db.Prepare("insert into resources (infoId, description, mimeType, size, uri, derefUri, digest) values (?, ?, ?, ?, ?, ?, ?)")
 				if err != nil {
 					log.Println(err)
 				}
-				res, err = statement.Exec(string(infoID), resource.Description, resource.MimeType, string(resource.Size), resource.URI, resource.Digest, resource.DerefURI)
+				_, err = statement.Exec(strconv.FormatInt(infoID, 10), resource.Description, resource.MimeType, string(resource.Size), resource.URI, resource.Digest, resource.DerefURI)
 				if err != nil {
 					log.Println("Insert resource error:", err)
-				} else {
-					resourceID, err = res.LastInsertId()
-					if err != nil {
-						log.Println(err)
-					}
-					fmt.Println("Added resource number", resourceID)
 				}
 			}
 
 			// add Areas
-			var areaID int64
 			for _, area := range info.Areas {
 				statement, err = db.Prepare("insert into areas (infoId, areaDesc, altitude, ceiling) values (?, ?, ?, ?)")
 				if err != nil {
 					log.Println(err)
 				}
-				res, err = statement.Exec(string(infoID), area.Description, area.Altitude, area.Ceiling)
+				res, err = statement.Exec(strconv.FormatInt(infoID, 10), area.Description, area.Altitude, area.Ceiling)
 				if err != nil {
 					log.Println("Insert area error:", err)
 				} else {
@@ -155,63 +139,41 @@ func ToDB(alert cap.Alert, xml string) {
 					if err != nil {
 						log.Println(err)
 					}
-					fmt.Println("Added area number", areaID)
 				}
 
 				// add Geocodes
-				var geocodeID int64
 				for _, param := range area.Geocodes {
 					statement, err = db.Prepare("insert into geocodes (areaId, valueName, value) values (?, ?, ?)")
 					if err != nil {
 						log.Println(err)
 					}
-					res, err = statement.Exec(string(areaID), param.ValueName, param.Value)
+					_, err = statement.Exec(strconv.FormatInt(areaID, 10), param.ValueName, param.Value)
 					if err != nil {
 						log.Println("Insert geocode error:", err)
-					} else {
-						geocodeID, err = res.LastInsertId()
-						if err != nil {
-							log.Println(err)
-						}
-						fmt.Println("Added geocode number", geocodeID)
 					}
 				}
 
 				// add Polygons
-				var polygonID int64
 				for _, polygon := range area.Polygons {
 					statement, err = db.Prepare("insert into polygons (areaId, polygon) values  (?, ?)")
 					if err != nil {
 						log.Println(err)
 					}
-					res, err = statement.Exec(string(areaID), polygon)
+					_, err = statement.Exec(strconv.FormatInt(areaID, 10), polygon)
 					if err != nil {
 						log.Println("Insert polygon error:", err)
-					} else {
-						polygonID, err = res.LastInsertId()
-						if err != nil {
-							log.Println(err)
-						}
-						fmt.Println("Added polygon number", polygonID)
 					}
 				}
 
 				// add Circles
-				var circleID int64
 				for _, circle := range area.Circles {
 					statement, err = db.Prepare("insert into circles (areaId, circle) values (?, ?)")
 					if err != nil {
 						log.Println(err)
 					}
-					res, err = statement.Exec(string(areaID), circle)
+					_, err = statement.Exec(strconv.FormatInt(areaID, 10), circle)
 					if err != nil {
 						log.Println("Insert circle error:", err)
-					} else {
-						circleID, err = res.LastInsertId()
-						if err != nil {
-							log.Println(err)
-						}
-						fmt.Println("Added circle number", circleID)
 					}
 				}
 			}
