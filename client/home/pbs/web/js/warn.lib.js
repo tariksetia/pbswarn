e2954e91e5a377b1b2fd265b7394fc08ff35d580
Art Botterell <art@botterell.net>
2019-05-13 15:24:55 -0700

    Update warn.lib.js

--- client/home/pbs/web/js/warn.lib.js
+++ client/home/pbs/web/js/warn.lib.js
@@ -1,16 +1,4 @@
-// 4/20/2019
-
-var alertObj
-var alertsObj = []
-var infosObj = []
-var extAlerts = []
-
-var timer
-var uptm 
-var clockTimer
-var stopCats
-var DateTime = luxon.DateTime
-
+// 5/12/2019
 
 function setLocalTimeZone(name) {
     DateTime.local().setZone(name)
@@ -53,100 +41,33 @@ async function showUptime() {
 
 }
 
-async function getUptime() {
-  const result = await $.ajax({
-    url: "/getUptime"
-})
-return JSON.parse(result)
-}
-
-$("#ExpiredCB").on('change', function () {
-  clearInterval(timer) // stop existing timer
-  clearDisplay()
-  updateDisplay()
-  $("#ExpiredCB").trigger('blur')
-  timer = setInterval(update, 6000) // launch new timer
-})
-
-$(".filter").on('change', function (e) {
-  clearInterval(timer) // stop existing timer
-  clearDisplay()
-  updateDisplay()
-  $(".filter").trigger('blur')
-  timer = setInterval(update, 3000) // launch new timer
-})
-
-$("#infoViewCloser").on('click', function () {
-  hideInfoView()
-})
-
-$("#masterDiv").on('click', function () {
-  hideInfoView()
-})
-
-// empty out the display of alerts
-function clearDisplay() {
-    //console.log("clearDisplay")
-    $("#masterDiv").empty()
-  }
-  
-// parse server data into an array of alerts (will be alertsObj)
-async function getAlerts() {
-    const result = await $.ajax({
-        url: "/getAlerts"
-    })
-    return JSON.parse(result)
-}
-
-// get areas for an info
-async function getAreas(infoID) {
-  const result = await $.ajax({
-    url: "/getAreas/" + infoID
-  })
-  return JSON.parse(result)
-}
-
-// get resources for an info
-async function getResources(infoID) {
-  const result = await $.ajax({
-    url: "/getResources/" + infoID
-  })
-  return JSON.parse(result)
-}
-
-// parse server data, return infosObj
-async function getAllInfos() {
-    const result = await $.ajax({
-      url: "/getAllInfos"
-    })
-    return JSON.parse(result)
-  }
-
+// load all infos from DB, then attach them to their corresponding alerts
 async function attachInfos(alertsObj) {
-    //console.log('[' + new Date().toUTCString() + '] ', "attachInfos")
-    infosObj = await getAllInfos()
-    // for each alert
-    for (var key in alertsObj) {
-        var alrt = alertsObj[key]
-        alrt.infos = []
-        // attach linked infos
-        for (var k2 in infosObj) {
+  infosObj = await getAllInfos(alertsObj[99].alertID)
+  // for each alert
+  for (var key in alertsObj) {
+      var alrt = alertsObj[key]
+      alrt.infos = []
+      // attach linked infos
+      for (var k2 in infosObj) {
         var info = infosObj[k2]
         if (info.alertID == alrt.alertID) {
             alrt.infos.push(info)
         }
-        }
-    } 
-    return alertsObj
+      }
+      
+  } 
+  return alertsObj
 }
   
-
+/*// force alerts into latest-first chronological order
 function sortAlertsBySentTime() {
     return  alertsObj.sort(function (a, b) {
-        var x = DateTime.fromISO(a["sent"]); var y = DateTime.fromISO(b["sent"]);
-        return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+        var x = DateTime.fromISO(a["sent"])
+        var y = DateTime.fromISO(b["sent"])
+        return ((x > y) ? -1 : ((x < y) ? 1 : 0))
     })
-}
+} */
 
 // determine if every info for a given alert is expired
 function isExpired(alert) {
@@ -154,12 +75,12 @@ function isExpired(alert) {
     for (var key in alert["infos"]) {
         var info = alert.infos[key]
         var expires = DateTime.fromISO(info["expires"])
-        //console.log("Expires", expires.toFormat("HH':'mm':'ss - LL'/'dd'/'yyyy ZZZZ"))
         return expires.ts < now.ts
     }
 }
 
-function getStopCats() {
+// return a string of the unchecked (blocked) categories
+function getStopCategories() {
     var cats = ""
     // get array of checkboxes
     var boxes = $(".filter")
@@ -174,86 +95,26 @@ function getStopCats() {
     return cats
 }
   
-
+// display a selected Info in detail
 async function showInfo(infoID) {
-    showInfoView()
-    $.ajax({
-      url: "/getInfo/" + infoID
-    }).then(async function (data) {
-      var info = JSON.parse(data)
-      var i = info[0]
-      // build info into HTML table
-      var infoTab = "<table id='infoTable'>"
-      infoTab = infoTab + `<tr><td></td><td>`
-      infoTab = infoTab + `<table><tr>
-      <td class='urgency ${i["urgency"]}'>${i["urgency"]} </td>
-      <td class='severity ${i["severity"]}'>${i["severity"]}</td>
-      <td class='certainty ${i["certainty"]}'>${i["certainty"]}</td>
-      </tr></table>`
-      infoTab = infoTab + `</td></tr>`
-      if (i.headline !="") {
-        infoTab = infoTab + `<tr><td class='label'>HEADLINE</td><td>${i.headline}</td></tr>`
-      }
-      if (i.cmam !="") {
-        infoTab = infoTab + `<tr><td class='label'>WEA&nbsp;TEXT</td><td>${i.cmam}</td></tr>`
-      }
-  
-      if (i.language != "") {
-        infoTab = infoTab + `<tr><td class='label'>LANG</td><td>${i.language}</td></tr>`
-      } 
-      if (i.responseType != "") {
-        infoTab = infoTab + `<tr><td class='label'>RESPONSE</td><td>${i.responseType} </td></tr>`
-      }
-      infoTab = infoTab + `
-      <tr><td class='label'>EVENT</td><td>${i.event}</td></tr>
-      <tr><td class='label'>SENDER</td><td>${i.senderName}</td></tr>
-      <tr><td class='label'>CATEGORY</td><td>${i.categories}</td></tr>
-      <tr><td class='label'>EXPIRES</td><td>${i.expires}</td></tr>`
-      if (i.description != "") {
-        infoTab = infoTab + 
-        `<tr><td class='label'>DESCRIPTION</td><td>${i.description.replace(/\n/g, "<br>")} </td></tr>`
-      }
-      if (i.description != "") {
-        infoTab = infoTab + 
-          `<tr><td class='label'>INSTRUCTION</td><td>${i.instruction.replace(/\n/g, "<br>")}</td></tr>`
-      }
-      if (i.contact != "") {
-        infoTab = infoTab + `<tr><td class='label'>CONTACT</td><td>${i.contact} </td></tr>`
-      }
-      if (i.web != "") {
-        infoTab = infoTab + `<tr><td class='label'>WEB</td><td>${i.web}</td></tr>`
-      }
-      // add areas
-      var areas = await getAreas(i.infoID)
-      for (var key in areas) {
-        var ar = areas[key]
-        if (ar.areaDesc != "" > 0) {
-          infoTab = infoTab + `<tr><td class='label'>AREA</td><td>${ar.areaDesc}</td></tr>`
-        }
-      }
-      // add resources
-      var resources = await getResources(i.infoID)
-      for (var key in resources) {
-        var resource = areas[key]
-        if (resource.length > 0) {
-          infoTab = infoTab + `<tr><td class='label'>RESOURCE</td><td>${resource.resourceDesc}</td></tr>`
-        }
-      }
-      // add link to raw
-
-      infoTab = infoTab + "</table>"
-      $("#infoDisplay").html(infoTab)
-    })
-  }
-
-async function viewArea(areaID) {
-
+  $("#infoViewer").show()
+  $.ajax({
+    url: "/getInfo/" + infoID
+  }).then(async function (data) {
+    var info = JSON.parse(data)
+    var html = await infoToInfoDisplayHTML(info[0])
+    $("#infoDisplay").html(html)
+  })
 }
 
-async function viewResource(areaID) {
-    
-}
+// display the raw CAP XML for an alert
+async function showRaw(alertID) {
+  console.log("Show Raw " + alertID)
+  // NEED TO ADD RAW PANEL TO HTML
+  // ALSO NEED TO ADD CLOSER BUTTON AND FUNCTION
 
-async function viewRaw(alertID) {
+  // SHOW THE RAW PANEL
+  $("#rawViewer").show()
+
 
 }
