7be62d1a0867b56a83fc50e16839dce513099e59
Art Botterell <art@botterell.net>
2019-08-13 16:49:42 -0700

    Update tuner.go

--- hdhr/tuner/tuner.go
+++ hdhr/tuner/tuner.go
@@ -4,7 +4,7 @@
  *  Contact: <warn@pbs.org>
  *  All Rights Reserved.
  *
- *  Updated 8/6/2019
+ *  Updated 8/13/2019
  *
  *************************************************************/
 
@@ -18,6 +18,7 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
+	"time"
 
 	config "github.com/Tkanos/gonfig"
 )
@@ -47,25 +48,26 @@ var freq string
 func Start() {
 	// get my ethernet IP address
 	myIP = getOutboundIP().String()
-	fmt.Println("(tuner.init) My address is", myIP)
+	log.Println("(tuner.Start) My address is", myIP)
 	// discover HDHomeRun receiver on ethernet
 	hdhr := strings.Split(Discover(), " ")
+	//fmt.Println("(tuner.Start) ", hdhr)
 	deviceID = hdhr[2]
 	deviceIP = hdhr[5]
-	fmt.Print("(tuner.init) Found receiver ", deviceID, " at ", deviceIP)
+	fmt.Print("(tuner.Start) Found receiver ", deviceID, " at ", deviceIP + ".")
 	// load system configuration
 	cfg = Configuration{}
 	if err := config.GetConf("warnmonitor.conf", &cfg); err != nil {
-		log.Println("(tuner.init config.GetConf)", err.Error())
+		log.Println("(tuner.Start config.GetConf)", err.Error())
 	}
 	targetPort = strconv.Itoa(cfg.UDPPort)
 	tuner = strconv.Itoa(cfg.Tuner)
 	pid = cfg.PID
 	freq = cfg.Freq
-	fmt.Println("(tuner.init) Tuner"+tuner+":", freq, "MHz, PID", pid)
+	fmt.Println("(tuner.Start) Tuner"+tuner+":", freq, "MHz, PID", pid)
 	// set station frequency
 	TuneRX(deviceID, tuner, freq)
-	fmt.Print("(tuner.init) Tuner status ", RXstatus(deviceID, tuner))
+	fmt.Print("(tuner.Start) Tuner status ", RXstatus(deviceID, tuner))
 	// set PID to monitor
 	SetPIDFilter(deviceID, tuner, pid)
 	// now launch UDP feed
@@ -73,16 +75,29 @@ func Start() {
 }
 
 func Discover() string {
-	return execCmd("/usr/local/bin/hdhomerun_config", "discover")
+	for {
+		discovery := execCmd("/usr/local/bin/hdhomerun_config", "discover")
+		fmt.Print("(tuner.Discover) ", discovery)
+		if (strings.Contains(discovery, "hdhomerun")) {
+			return discovery
+		}
+		fmt.Println("(tuner.Discover) retry")
+		time.Sleep(2 * time.Second)
+	}
 }
 
 func TuneRX(device string, tuner string, freq string) {
+	device = strings.TrimSuffix(device, "\n")
+	if (device == "") {
+		fmt.Println("(tuner.TuneRX) no device ID provided: ", device)
+		return
+	}
 	fmt.Println("(tuner.TuneRX) set device", device, "tuner", tuner, "to", freq)
 	execCmd("/usr/local/bin/hdhomerun_config", device, "set", "/tuner"+tuner+"/channel", "8vsb:"+freq+"000000")
 	// load system configuration
 	cfg = Configuration{}
 	if err := config.GetConf("warnmonitor.conf", &cfg); err != nil {
-		log.Println("(tuner.init config.GetConf)", err.Error())
+		fmt.Println("(tuner.TuneRX config.GetConf)", err.Error())
 	}
 	SetPIDFilter(device, tuner, cfg.PID)
 	TargetRX(device, tuner, getOutboundIP().String(), strconv.Itoa(cfg.UDPPort))
@@ -105,9 +120,11 @@ func execCmd(cmdStr string, args ...string) string {
 	cmd := exec.Command(cmdStr, args...)
 	var out bytes.Buffer
 	cmd.Stdout = &out
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
 	err := cmd.Run()
 	if err != nil {
-		log.Println("(execCmd)", err)
+		fmt.Println("(execCmd) " + fmt.Sprint(err) + ": " + stderr.String())
 	}
 	return out.String()
 }
